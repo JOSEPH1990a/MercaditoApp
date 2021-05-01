988bd1d98567943b6b654484b0a39bd65f69d538
blanca <Kristel240298@gmail.com>
2021-04-30 23:58:21 -0500

    se agrego la tercera pantalla y se agrego color y el boton de empezar

---
+++ app/src/main/java/com/codersof/mercaditoapp/tour_activity_tres.java
@@ -0,0 +1,14 @@
+package com.codersof.mercaditoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tour_activity_tres extends AppCompatActivity {
+
+    @Override
+    protected void onCreate ( Bundle savedInstanceState ){
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_tour_tres);
+    }
+}
